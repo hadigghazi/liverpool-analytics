77b37584ade408a6c99cb452a3a965cb70e609c7
hadyGhazi <hghazi@capadev.com>
2026-04-25 17:34:23 +0300

    chore: switch to 2526 season

--- scraper/run_weekly.bat
+++ scraper/run_weekly.bat
@@ -1,7 +1,7 @@
 @echo off
 cd C:\Users\User\liverpool-analytics\scraper
 set GCP_PROJECT=liverpool-analytics
-set CURRENT_SEASON=2425
-python scrape_liverpool.py 2425
+python scrape_liverpool.py --current
 cd ..\dbt
 dbt run --profiles-dir . --project-dir .
+echo Done at %date% %time%
